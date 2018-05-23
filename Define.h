12c568526b2cee40ccd6fd2b86a5c21846335af8
Yuri Sanas <sanasy.yuri@gmail.com>
2018-05-23 08:30:48 -0300

    Create Define.h

---
+++ Define.h
@@ -0,0 +1,87 @@
+#define	ELESIS_FIRST	0
+#define	ELESIS_SECOND	20
+#define	ELESIS_THIRD	38
+#define	ELESIS_FOURTH	53
+
+#define	LIRE_FIRST	1
+#define	LIRE_SECOND	21
+#define	LIRE_THIRD	39
+#define	LIRE_FOURTH	54
+
+#define	ARME_FIRST	2
+#define	ARME_SECOND	22
+#define	ARME_THIRD	40
+#define	ARME_FOURTH	55
+
+#define	LASS_FIRST	3
+#define	LASS_SECOND	23
+#define	LASS_THIRD	41
+#define	LASS_FOURTH	56
+
+#define	RYAN_FIRST	4
+#define	RYAN_SECOND	24
+#define	RYAN_THIRD	42
+#define	RYAN_FOURTH	57
+
+#define	RONAN_FIRST	5
+#define	RONAN_SECOND	25
+#define	RONAN_THIRD	43
+#define	RONAN_FOURTH	58
+
+#define	AMY_FIRST	6
+#define	AMY_SECOND	26
+#define	AMY_THIRD	44
+#define	AMY_FOURTH	59
+
+#define	JIN_FIRST	7
+#define	JIN_SECOND	27
+#define	JIN_THIRD	45
+#define	JIN_FOURTH	60
+
+#define	SIEGHART_FIRST	8
+#define	SIEGHART_SECOND	28
+#define	SIEGHART_THIRD	46
+#define	SIEGHART_FOURTH	61
+
+#define	MARI_FIRST	9
+#define	MARI_SECOND	29
+#define	MARI_THIRD	47
+#define	MARI_FOURTH	62
+
+#define	DIO_FIRST	10
+#define	DIO_SECOND	30
+#define	DIO_THIRD	48
+#define	DIO_FOURTH	63
+
+#define	ZERO_FIRST	11
+#define	ZERO_SECOND	31
+#define	ZERO_THIRD	49
+#define	ZERO_FOURTH	64
+
+#define	REY_FIRST	12
+#define	REY_SECOND	32
+#define	REY_THIRD	50
+#define	REY_FOURTH	65
+
+#define	LUPUS_FIRST	13
+#define	LUPUS_SECOND	33
+#define	LUPUS_THIRD	51
+#define	LUPUS_FOURTH	66
+
+#define	LIN_FIRST	14
+#define	LIN_SECOND	34
+#define	LIN_THIRD	52
+#define	LIN_FOURTH	67
+
+#define	AZIN_FIRST	15
+#define	AZIN_SECOND	35
+
+#define	HOLY_FIRST	16
+#define	HOLY_SECOND	36
+
+#define	EDEL_FIRST	17
+#define	EDEL_SECOND	37
+
+#define	VEIGAS_FIRST	18
+
+#define	UNO_FIRST	19
